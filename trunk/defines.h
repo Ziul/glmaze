5bb38c0060fb6fd189d88dd983afaccc9d97befe
Luiz Oliveira <ziuloliveira@gmail.com>
2012-04-21 00:35:34 +0000

    Agora vai

--- trunk/defines.h
+++ trunk/defines.h
@@ -1,14 +1,21 @@
 #ifndef __DEFINESS__H_
 #define __DEFINESS__H_
 
-#ifdef _WIN32
-    #define WIN32_LEAN_AND_MEAN
-    #include <windows.h>
+
+#if defined (__APPLE__) || defined (MACOSX) /*MAC OS*/
+    #include <GLUT/glut.h>
+#else
+    #ifdef _WIN32                           /* Windows */
+    	#define WIN32_LEAN_AND_MEAN
+        #include <glee.h>
+        #include <gl/glut.h>
+        #include <windows.h>
+    #else                                   /*Linux*/
+        #include <GL/gl.h>
+        #include <GL/glut.h>
+        #include <GL/glu.h>
     #endif
-
-#include <gl/glut.h>
-#include <gl/gl.h>
-
+#endif
 
 #define SCREEN_WIDTH            640
 #define SCREEN_HEIGHT           480
