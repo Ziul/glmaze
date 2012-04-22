abf1cb01c000784d06929b2b59c35c4cea777164
Gustavo Jaruga Cruz <darksshades@gmail.com>
2012-04-22 20:02:31 +0000

    Corrigido erros de compilação.

--- trunk/entidade.cpp
+++ trunk/entidade.cpp
@@ -1,18 +1,18 @@
 #include "entidade.h"
 
 
-static std::vector<Entidade*> Entidade::EntidadeList;
+std::vector<Entidade*> Entidade::EntidadeList;
 
-Entidade::Entidade();
-Entidade::~Entidade();
-void Entidade::calculaColisao(Entidade* objeto);
-void Entidade::calculaColisaoMapa();
+Entidade::Entidade(){}
+Entidade::~Entidade(){}
+void Entidade::calculaColisao(Entidade* objeto){}
+void Entidade::calculaColisaoMapa(){}
 
-void Entidade::removeFromEntidadeList();
+void Entidade::removeFromEntidadeList(){}
 
-bool Entidade::carregaModelo(char* file);
-void Entidade::loop();
-void Entidade::render();
-void Entidade::cleanup();
-void Entidade::executaColisao();
-void Entidade::testaColisao();
+bool Entidade::carregaModelo(char* file){return true;}
+void Entidade::loop(){}
+void Entidade::render(){}
+void Entidade::cleanup(){}
+void Entidade::executaColisao(){}
+void Entidade::testaColisao(){}
